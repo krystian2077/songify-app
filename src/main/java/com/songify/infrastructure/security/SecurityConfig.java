e1714a8b006b7a09ff1f4f29e6461b2bea8af5f9
Krystian Potaczek <krystian.potaczek07@gmail.com>
2025-04-15 19:55:24 +0200

    Added spring security

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.songify.infrastructure.security;
 
 import com.songify.domain.usercrud.UserRepository;
+import com.songify.infrastructure.security.jwt.JwtAuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 class SecurityConfig {
@@ -16,7 +25,47 @@ class SecurityConfig {
     }
 
     @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthTokenFilter jwtAuthTokenFilter) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.sessionManagement(
+                c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-resources/**").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/users/register/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/token/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/songs/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/artists/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/albums/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/genres/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/songs/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/artists/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/artists/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/artists/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/artists/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/albums/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/albums/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/genres/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/users/**").hasRole("ADMIN")
+                .anyRequest().authenticated());
+
+        return http.build();
     }
 }
